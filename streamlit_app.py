707a5f76a56ee09957076041ae0532eb1066ee5e
vvsravani357 <velpulavenkatasravani@gmail.com>
2026-06-11 14:32:09 +0530

    Update streamlit_app.py
    Created SmoothieFroot Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 # Write directly to the app
-st.title("My Parents New Healthy Diner")
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
@@ -35,4 +34,5 @@ if ingredients_list:
 #New section to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json)
+#st.text(smoothiefroot_response.json)
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
