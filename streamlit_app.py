74e27660ffc6c9faf9ff0860c221d368734d1a63
vvsravani357 <velpulavenkatasravani@gmail.com>
2026-06-11 13:09:44 +0530

    Update streamlit_app.py
    Added a new line My Parents New Healthy Diner

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 # Write directly to the app
+streamlit.title("My Parents New Healthy Diner")
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
