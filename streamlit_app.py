b61141d162eef4bbf59896af4a9d028b771b846d
vvsravani357 <velpulavenkatasravani@gmail.com>
2026-06-11 14:18:58 +0530

    Update streamlit_app.py
    rectified the link

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,5 +34,5 @@ if ingredients_list:
         st.success("Your Smoothie is ordered, " + name_on_order + "!", icon="✅")
 #New section to display smoothiefroot nutrition information
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
